36d152b001173462fe1b74ac9b393ba7d4b3ac9e
Blackscarft <123agusnugroho123@gmail.com>
2021-07-25 19:42:26 +0700

    make welcome message to dashboard

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -3,11 +3,20 @@
 
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800"><?= $title ?></h1>
-
+    <div class="card">
+        <div class="card-header">
+            Welcome
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Sistem Memo Perintah Kerja</h5>
+            <p class="card-text">Selamat datang Super Administrator di sistem MPK</p>
+            <a href="<?= base_url('/mpk/data') ?>" class="btn btn-primary">Data MPK</a>
+        </div>
+    </div>
     <!-- page content -->
 
 </div>
 <!-- /.container-fluid -->
 
 </div>
-<!-- End of Main Content --> 
+<!-- End of Main Content -->
